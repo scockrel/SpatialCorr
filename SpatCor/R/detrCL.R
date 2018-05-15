4e759d12949537ef09f424b3e37bb91b64a09d4b
scockrel <scockrel@mix.wvu.edu>
2018-05-15 12:26:17 -0400

    detrend climate data

---
+++ SpatCor/R/detrCL.R
@@ -0,0 +1,28 @@
+#' Dating of netcdf Files and creating a seasonal compilation
+#'
+#' This function allows you to create spatial field correlations with tree rings and
+#' gridded climate data
+#' @param climData climate data
+#' @param mod mode of detrending
+#' @return results from datM
+#' @export
+#' 
+
+detrCL <- function(climData, mod){ #mod is fd or lm
+  for (i in unique(substring(climData, 7))){
+    d <- values(subset(datM, grep(i, names(datM), value=T)))
+    d[is.na(d[])] <- -9999
+    if(mod == "fd"){
+    ## First Differences Method
+    r <- t(diff(t(d), 1))
+    do.call("<<-", list(i, r))
+    }else{
+      if(mod == "lm"){
+    # Linear Model Method
+        lm_x <- seq(1:dim(get(i))[2])
+        r <- t(resid(lm(t(get(i)) ~ lm_x)))
+        do.call("<<-", list(i, r))
+      }else{
+        do.call("<<-", list(i, d))
+  }
+}
